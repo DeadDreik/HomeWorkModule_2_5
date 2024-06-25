22c2cd1a6d05fd0d939183c05bde2cb28e6934db
Shamil <65324697+DeadDreik@users.noreply.github.com>
2024-06-25 22:43:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        row = []
+        for j in range(m):
+            row.append(value)
+        matrix.append(row)
+    return matrix
+result = get_matrix(5, 6, 10)
+print(get_matrix(3,5, 42))
